a7554401a69bfa4f99cc6870de28285c722deb8f
WahdKhona <wahdkhonafdar@gmail.com>
2026-01-22 17:28:38 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Optional: Discord button glowing pulse
-const discordBtn = document.getElementById('discord-btn');
-setInterval(() => {
-    discordBtn.style.boxShadow = `0 0 20px rgba(114,137,218,${Math.random()})`;
-}, 500);
